da24a6864844bd069a8738a38dba38b70b1cfd1d
Teddychuks <chukwukaohuruogu@gmail.com>
2025-03-11 18:19:56 +0100

    test integration

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -26,7 +26,8 @@ import { Todo } from '../entity/todo.entity';
             package: 'todo',
             protoPath: join(__dirname, '../grpc/proto/todo.proto'),
             // url: configService.get('SERVICE_A_GRPC_URL', 'localhost:5001')
-            url: "172.31.33.214:5001"
+            // url: "172.31.33.214:5001"
+            url: 'dns:///grpc-service-876551831298.us-central1.run.app:443',
           },
         }),
       },
